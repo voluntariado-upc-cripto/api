cc0f3cd0f6a0109cc08838f3d347fbc3bf9b3105
Salvador Diaz Aguirre <u202216148@upc.edu.pe>
2024-08-11 16:51:10 -0500

    feat: tipso de hash.

--- api/server.js
+++ api/server.js
@@ -24,6 +24,20 @@ app.use(middlewares);
 //ids:
 
 // Endpoints personalizados:
+function simpleHash(password) {
+  let hash = 0;
+  
+  // Iteramos sobre cada carácter de la contraseña
+  for (let i = 0; i < password.length; i++) {
+      const char = password.charCodeAt(i);
+      // Realizamos una operación matemática simple para generar un hash
+      hash = (hash << 5) - hash + char;
+      hash = hash & hash; // Convertir a un entero de 32 bits
+  }
+
+  // Convertimos el hash a hexadecimal
+  return hash.toString(16);
+}
 
 // GET accounts
 app.get('/api/v1/accounts', (req, res) => {
@@ -32,7 +46,8 @@ app.get('/api/v1/accounts', (req, res) => {
   res.json(accounts);
 });
 
-app.post('/api/v1/accounts', async (req, res) => {
+//POST accounts 1
+app.post('/api/v1/accounts/manual_hash', async (req, res) => {
   console.log("Datos recibidos en /api/v1/accounts:", req.body);
   const db = jsonServerRouter.db;
   const { email, pass } = req.body;
@@ -47,7 +62,32 @@ app.post('/api/v1/accounts', async (req, res) => {
   const newAccount = {
     _id: uuidv4(),
     email,
-    pass,
+    pass:simpleHash(pass),
+    type:0,
+  };
+
+  db.get('accounts').push(newAccount).write();
+  res.status(201).json(newAccount);
+});
+
+//POST accounts 2
+app.post('/api/v1/accounts/bcrypt_hash', async (req, res) => {
+  console.log("Datos recibidos en /api/v1/accounts:", req.body);
+  const db = jsonServerRouter.db;
+  const { email, pass } = req.body;
+
+  // Verifica si el usuario o el email ya existen
+  const existingUser = db.get('accounts').find(account=>account.email===email).value();
+  if (existingUser) {
+    return res.status(400).json({ error: 'User or email already exists' });
+  }
+
+  saltRounds=10;
+  const newAccount = {
+    _id: uuidv4(),
+    email,
+    pass:await bcrypt.hash(pass, saltRounds),
+    type:1,
   };
 
   db.get('accounts').push(newAccount).write();
@@ -63,8 +103,23 @@ app.post('/api/v1/accounts/login', (req, res) => {
   const account = db.get('accounts').find(account =>account.email === email).value();
 
   if (account) {
-    if (pass==account.pass) {
-      res.json({ message: 'Login successful', email: account.email, _id: account._id });
+    if(account.type===0){
+      const hashedPass=simpleHash(pass);
+      if(hashedPass===account.pass){
+        res.json({ message: 'Login successful', email: account.email, _id: account._id });
+      }
+      else{
+        res.status(401).json({ error: 'Invalid credentials' });
+      }
+    }
+    else if(account.tyoe===1){
+      const validPass=bcrypt.compareSync(pass, account.pass);
+      if(validPass){
+        res.json({ message: 'Login successful', email: account.email, _id: account._id });
+      }
+      else{
+        res.status(401).json({ error: 'Invalid credentials' });
+      }
     }
     else {
       res.status(401).json({ error: 'Invalid credentials' });
